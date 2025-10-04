be481a1428cc13ab1d21307f065325bc0fc31c96
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-10-04 17:32:03 -0300

    added first calculation method to usecase

---
+++ app/bin/main/org/lseixas/domain/enums/StatusApenado.kt
@@ -0,0 +1,9 @@
+package org.lseixas.domain.enum
+
+/**
+ * Enum para representar o status do apenado (primário ou reincidente).
+ */
+enum class StatusApenado { 
+    PRIMARIO, 
+    REINCIDENTE 
+}

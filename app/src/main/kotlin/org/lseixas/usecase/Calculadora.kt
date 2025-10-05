a2dc4c39c7cdc82142a6a7012f3801a4a510405b
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-10-05 11:46:14 -0300

    fixed enum directories, fixed calculadora usecase, finished calculadora logic

--- app/src/main/kotlin/org/lseixas/usecase/Calculadora.kt
+++ app/src/main/kotlin/org/lseixas/usecase/Calculadora.kt
@@ -1,44 +1,92 @@
 package org.lseixas.usecase
 
-import java.time.LocalDate
 import org.lseixas.domain.enum.TipoCrime
 import org.lseixas.domain.enum.StatusApenado
+import org.lseixas.domain.objects.UsuarioES
+import java.time.LocalDate
+
+
+data class ResultadoProgressao(
+    val diasParaProgredir: Int,
+    val fracaoUtilizada: Float
+)
 
 fun calcularBeneficios(entrada: UsuarioES): UsuarioES {
-    
-    val penaBaseDias: Int = 
-        (UsuarioES.penaAnos * 365) + 
-        (UsuarioES.penaMeses * 30) + 
-        (UsuarioES.penaDias) - /* As tres primeiras linhas representam a pena total em dias */
-        (UsuarioES.detracaoDias) /* Para o calculo deve-se subtrair a detração */
 
+
+    // Semiaberto e Aberto
+    val penaBaseDias: Int = (entrada.penaAnos * 365) + (entrada.penaMeses * 30) + (entrada.penaDias) - (entrada.detracaoDias)
+    val resultadoSemiAberto: ResultadoProgressao = calcularDataProgressaoSemiaberto(penaBaseDias = penaBaseDias, entrada = entrada)
+    val dataDoSemiAberto: LocalDate = entrada.dataInicioPena.plusDays(resultadoSemiAberto.diasParaProgredir.toLong())
+    val penaRestanteEmDias = penaBaseDias - resultadoSemiAberto.diasParaProgredir
+    val diasAdicionaisParaOAberto = (penaRestanteEmDias * resultadoSemiAberto.fracaoUtilizada).toInt()
+    val dataDoAberto: LocalDate = dataDoSemiAberto.plusDays(diasAdicionaisParaOAberto.toLong())
+
+
+    // Livramento
+    val diasParaLivramento: Int = calcularDataLivramento(
+        penaBaseDias = penaBaseDias,
+        entrada = entrada
+    )
+    val dataLivramentoFinal: LocalDate?
+    if (diasParaLivramento > 0) {
+        dataLivramentoFinal = entrada.dataInicioPena.plusDays(diasParaLivramento.toLong())
+    } else {
+        dataLivramentoFinal = null
+    }
+
+
+    return entrada.copy(
+        dataProgressaoSemiaberto = dataDoSemiAberto,
+        dataProgressaoAberto = dataDoAberto,
+        dataLivramentoCondicional = dataLivramentoFinal
+    )
 }
 
-fun calcularDataProgressaoSemiaberto(penaBaseDias: Int, entrada: UsuarioES): Int {
+fun calcularDataLivramento(penaBaseDias: Int, entrada: UsuarioES): Int {
 
-    val fracaoProgressao: Float = when (entrada.TipoCrime) {
-        COMUM -> when (entrada.StatusApenado) {
-            PRIMARIO -> 0.16f
-            REINCIDENTE -> 0.2f
+    val penaResultado: Int = when (entrada.tipoCrime) {
+        TipoCrime.HEDIONDO_EQUIPARADO -> when (entrada.ehHediondoComMorte) {
+            true -> 0
+            false -> (penaBaseDias * (2.0 / 3.0)).toInt()
         }
-        VIOLENCIA_AMEACA -> when (entrada.StatusApenado) {
-            PRIMARIO -> 0.25f
-            REINCIDENTE -> 0.3f
+        else -> when (entrada.statusApenado) {
+            StatusApenado.REINCIDENTE -> (penaBaseDias * 0.5).toInt()
+            StatusApenado.PRIMARIO -> (penaBaseDias * (1.0 / 3.0)).toInt()
         }
-        HEDIONDO_EQUIPARADO -> when (entrada.StatusApenado) {
-            PRIMARIO -> when (entrada.ehHediondoComMorte) {
-                false -> 0.4f
-                true -> 0.5f
+    }
+
+    return penaResultado
+}
+
+fun calcularDataProgressaoSemiaberto(penaBaseDias: Int, entrada: UsuarioES): ResultadoProgressao {
+
+    val fracaoProgressao: Float = when (entrada.tipoCrime) {
+        TipoCrime.COMUM -> when (entrada.statusApenado) {
+            StatusApenado.PRIMARIO -> 0.16f
+            StatusApenado.REINCIDENTE -> 0.20f
+        }
+        TipoCrime.VIOLENCIA_AMEACA -> when (entrada.statusApenado) {
+            StatusApenado.PRIMARIO -> 0.25f
+            StatusApenado.REINCIDENTE -> 0.30f
+        }
+        TipoCrime.HEDIONDO_EQUIPARADO -> when (entrada.statusApenado) {
+            StatusApenado.PRIMARIO -> when (entrada.ehHediondoComMorte) {
+                false -> 0.40f
+                true -> 0.50f
             }
-            REINCIDENTE -> when(entrada.ehHediondoComMorte) {
-                false -> 0.6f
-                true -> 0.7f
+            StatusApenado.REINCIDENTE -> when (entrada.ehHediondoComMorte) {
+                false -> 0.60f
+                true -> 0.70f
             }
         }
     }
 
-    val diasProgredir = (penaBaseDias * fracaoProgressao).toInt()
+    val diasParaProgredir = (penaBaseDias * fracaoProgressao).toInt()
 
-    return entrada.dataInicioPena.plusDays(diasProgredir.toLong())
+    return ResultadoProgressao(
+        diasParaProgredir = diasParaProgredir,
+        fracaoUtilizada = fracaoProgressao
+    )
 
 }
